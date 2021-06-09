639a9a3784da95e24ef60237405f97e130bd96da
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2021-06-09 12:14:29 +0200

    minor stuff

---
+++ pratice.py
@@ -0,0 +1,19 @@
+#import matplotlib.pyplot as plt
+#import numpy as np
+
+msg = "hello"
+print(msg)
+
+#x = np.linspace(0,20,100)
+#plt.plot(x,np.sin(x))
+#plt.show()
+if 5 > 2: 
+    print("Five")
+numberValue = int(3)
+stringValue = str(3)
+floatValue = float(3)
+unknownValue = 3
+print(type(numberValue))
+print(type(stringValue))
+print(type(floatValue))
+print(type(unknownValue))
